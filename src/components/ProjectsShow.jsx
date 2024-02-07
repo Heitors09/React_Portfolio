c59dd3b84201b68d9359043ca75344e401c62a1f
Heitor <heitorao32@gmail.com>
2024-02-07 15:29:58 -0300

    search-placeholder-fix

--- src/components/ProjectsShow.jsx
+++ src/components/ProjectsShow.jsx
@@ -8,7 +8,7 @@ export function ProjectsShow() {
         <div className="flex w-[843px] h-20 bg-bgcolor drop-shadow-default rounded-lg justify-between items-center ">
           <textarea
             placeholder="Pesquise..."
-            className="bg-transparent resize-none outline-none w-[700px] h-20 ml-5 mt-4  text-green-500 "
+            className="bg-transparent resize-none outline-none w-[700px] h-20 ml-5 mt-[60px]  text-green-500 "
           />
           <Search className="mr-5 size-6 text-boxTxColor hover:text-green-500 cursor-pointer" />
         </div>
